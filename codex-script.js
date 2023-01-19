da08d4b5a08e2a00c6216b4c62ce383538ba8c78
Tobias Masters <tobymasters10@gmail.com>
2023-01-19 12:36:24 +0000

    Wrote basic fetch

--- codex-script.js
+++ codex-script.js
@@ -1,3 +1,15 @@
-window.onload = function () {
+var flag = "primary-entries"
 
+window.onload = function () {
+  renderMenu();
+}
+
+function renderMenu() {
+  $.getJSON(
+    "codex-data.json", function(result) {
+      $.each(result, function(key, value) {
+        window.alert(value.category);
+      })
+    }
+  );
 }
